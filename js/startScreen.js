eff481540fe33ce30fc54cdac1dfb222cb3702f1
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-06 16:16:40 -0700

    name changes and commented code

--- js/startScreen.js
+++ js/startScreen.js
@@ -1,16 +1,24 @@
 $(document).ready(function()
 {
+  // start game
   $("#topStart").click(function()
   {
-    var startGame = new StartGame();
-    startGame.start();
+    // remove start screen
+    $("#startScreen").remove();
+    // create game screen
+    var gameScreen = new GameScreen();
+    gameScreen.makeGameScreen();
+    // start game
+    var game = new MainGame();
+    game.start();
   });
+  // view instructions
   $("#bottomInstructions").click(function()
   {
     instructions();
   });
   function instructions()
   {
-    
+    // implement instructions here
   }
 });
