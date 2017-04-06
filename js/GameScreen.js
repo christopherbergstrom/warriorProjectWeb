ded2820d5ceab85c3552d437d91b7ee27071ecb2
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-06 16:17:22 -0700

    Add files via upload

---
+++ js/GameScreen.js
@@ -0,0 +1,11 @@
+class GameScreen
+{
+  makeGameScreen()
+  {
+    // make divs for game screen
+    $("body").append("<div id='leftBar'></div>");
+    $("body").append("<div id='rightBar'></div>");
+    $("body").append("<div id='middle'></div>");
+    $("body").append("<div id='bottom'></div>");
+  }
+}
