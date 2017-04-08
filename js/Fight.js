b0078b8f2d5250feb8dd0ba7547ffa3acf4ec7c8
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-08 09:51:21 -0700

    starting fight.js

---
+++ js/Fight.js
@@ -0,0 +1,11 @@
+class Fight
+{
+  // player fights enemy
+  // this is where all dom manipulation for fight happens
+  fight(player, enemy)
+  {
+    console.log("in fight");
+    console.log(player.getName());
+    console.log(enemy.getName());
+  }
+}
