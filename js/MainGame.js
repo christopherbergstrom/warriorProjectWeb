8e4733914cb6e7452a1633799ef5ebfdd44a77d8
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-08 09:52:15 -0700

    testing fight.js
    First try worked, which kind of makes me nervous haha

--- js/MainGame.js
+++ js/MainGame.js
@@ -1,8 +1,11 @@
-class StartGame
+class MainGame
 {
   start()
   {
-    var player = new Player("Chris", "sword", 1);
-    console.log(player);
+    var fight = new Fight();
+    // name, health, lightWpn, lightDmg, lightWpnDmgRng, mediumWpn, mediumDmg, mediumWpnDmgRng, heavyWpn, heavyDmg, heavyWpnDmgRng, shield, shieldDmg, shieldDmgRng, power, magic, magicDmg, magicDmgRng, dodge, isDragon, fireBreath, fireBreathDmg, fireBreathDmgRng, gold
+    var player = new Character("Warrior", 250, "Blow Darts", 1, 2,"Chain Whip", 7, 3, "Shortsword", 15, 4, "Wood Shield", 2, 3, "null", "null", 0, 0, 10, false, "null", 0, 0, 0);
+    var enemy = new Character("Skeleton Archer", 250, "Dagger", 1, 3, "Throwing Knives", 4, 4, "Bow", 8, 3, "Shield", 1, 5, "null", "null", 0, 0, 20, false, "null", 0, 0, 0);
+    fight.fight(player, enemy);
   }
 }
