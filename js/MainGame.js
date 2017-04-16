834e85f19df6da14ce50e110f3083f019784d40b
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-15 22:30:35 -0700

    update for character level addition

--- js/MainGame.js
+++ js/MainGame.js
@@ -2,10 +2,15 @@ class MainGame
 {
   start()
   {
+    // below is test code for fight
     var fight = new Fight();
-    // name, health, lightWpn, lightDmg, lightWpnDmgRng, mediumWpn, mediumDmg, mediumWpnDmgRng, heavyWpn, heavyDmg, heavyWpnDmgRng, shield, shieldDmg, shieldDmgRng, power, magic, magicDmg, magicDmgRng, dodge, isDragon, fireBreath, fireBreathDmg, fireBreathDmgRng, gold
-    var player = new Character("Warrior", 250, "Blow Darts", 1, 2,"Chain Whip", 7, 3, "Shortsword", 15, 4, "Wood Shield", 2, 3, "null", "null", 0, 0, 10, false, "null", 0, 0, 0);
-    var enemy = new Character("Skeleton Archer", 250, "Dagger", 1, 3, "Throwing Knives", 4, 4, "Bow", 8, 3, "Shield", 1, 5, "null", "null", 0, 0, 20, false, "null", 0, 0, 0);
+    // name, health, lightWpn, lightDmg, lightWpnDmgRng, mediumWpn, mediumDmg, mediumWpnDmgRng, heavyWpn, heavyDmg, heavyWpnDmgRng, shield, shieldDmg, shieldDmgRng, power, powerDmg, magic, magicDmg, magicDmgRng, dodge, isDragon, fireBreath, fireBreathDmg, fireBreathDmgRng, gold, lebel
+    var player = new Character("Warrior", 250, "Blow Darts", 1, 2,"Chain Whip", 7, 3, "Shortsword", 15, 4, "Wood Shield", 2, 3, "Ice ", 10, "Ice Blast", 10, 10, 10, false, null, 0, 0, 0, 1);
+    var enemy = new Character("Skeleton Archer", 250, "Dagger", 1, 3, "Throwing Knives", 4, 4, "Bow", 8, 3, "Shield", 1, 5, null, 0, null, 0, 0, 20, false, null, 0, 0, 0, 1);
     fight.fight(player, enemy);
+  }
+  returnToCity()
+  {
+    // implement city fuctions here, talk to people, weapon shop, practice arena, instructions
   }
 }
