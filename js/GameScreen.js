ac428386ddf2738f23fcd989401f9ee2619c36e0
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-18 22:07:13 -0700

    Update GameScreen.js

--- js/GameScreen.js
+++ js/GameScreen.js
@@ -1,5 +1,10 @@
 class GameScreen
 {
+  constructor(player, enemy)
+  {
+    this.player = player;
+    this.enemy = enemy;
+  }
   makeGameScreen()
   {
     // make divs for game screen
@@ -11,5 +16,16 @@ class GameScreen
     $("body").append("<div id='enemyStats'></div>");
     $("body").append("<div id='screen'></div>");
     $("body").append("<div id='actions'></div>");
+  }
+  makeCity()
+  {
+    var battle = "<div class='col-md-4 topAndBottom topButtons text'>Battle</div>";
+    var practiceArena = "<div class='col-md-4 topAndBottom topButtons text'>Practice Arena</div>";
+    var weaponShop = "<div class='col-md-4 topAndBottom topButtons text'>Weapon Shop</div>";
+    var talkToLocals = "<div class='col-md-6 topAndBottom bottomButtons text'>Talk to Locals</div>";
+    var instructions = "<div class='col-md-6 topAndBottom bottomButtons text'>Instructions</div>";
+    $("body").append("<div class='container-fluid'></div>")
+    $(".container-fluid").append("<div class='row'>"+battle+practiceArena+weaponShop+"</div>");
+    $(".container-fluid").append("<div class='row'>"+talkToLocals+instructions+"</div>");
   }
 }
