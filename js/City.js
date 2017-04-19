8aec005ddaf87dc6bf6f99310a023a36afae3d41
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-18 22:06:43 -0700

    starting to implement menu

--- js/City.js
+++ js/City.js
@@ -1,16 +1,47 @@
-class MainGame
+class City
 {
   start()
   {
+    // intro here
+    
     // below is test code for fight
     var fight = new Fight();
-    // name, health, lightWpn, lightDmg, lightWpnDmgRng, mediumWpn, mediumDmg, mediumWpnDmgRng, heavyWpn, heavyDmg, heavyWpnDmgRng, shield, shieldDmg, shieldDmgRng, power, powerDmg, magic, magicDmg, magicDmgRng, dodge, isDragon, fireBreath, fireBreathDmg, fireBreathDmgRng, gold, lebel
-    var player = new Character("Warrior", 250, "Blow Darts", 1, 2,"Chain Whip", 7, 3, "Shortsword", 15, 4, "Wood Shield", 2, 3, "Ice ", 10, "Ice Blast", 10, 10, 10, false, null, 0, 0, 0, 1);
-    var enemy = new Character("Skeleton Archer", 250, "Dagger", 1, 3, "Throwing Knives", 4, 4, "Bow", 8, 3, "Shield", 1, 5, null, 0, null, 0, 0, 20, false, null, 0, 0, 0, 1);
-    fight.fight(player, enemy);
+    var createCharacter = new CreateCharacter();
+    var weaponShop = new WeaponShop();
+    var gameScreen = new GameScreen();
+    var instructions = new Instructions();
+    var player = new Character("Warrior", 250, "Blow Darts", 1, 2, "Chain Whip", 7, 3, "Shortsword", 15, 4, "Wood Shield", 2, 3, "Ice ", 10, "Ice Blast", 10, 10, 10, false, null, 0, 0, 0, 1);
+    // fight.fight(createCharacter.player(), createCharacter.practice());
+    // fight.fight(player, createCharacter.practice(player.getHealth(), player.getLightDmg(), player.getLightRng(), player.getMediumDmg(), player.getMediumRng(), player.getHeavyDmg(), player.getHeavyRng(), player.getShieldDmg(), player.getShieldRng(), player.getDodge()));
+    this.returnToCity(player, createCharacter, fight, weaponShop, gameScreen, instructions);
   }
-  returnToCity()
+  returnToCity(player, createCharacter, fight, weaponShop, gameScreen, instructions)
   {
+    gameScreen.makeCity();
+    // fight.fight(player, createCharacter.one());
+    // fight.fight(player, createCharacter.practice(player.getHealth(), player.getLightDmg(), player.getLightRng(), player.getMediumDmg(), player.getMediumRng(), player.getHeavyDmg(), player.getHeavyRng(), player.getShieldDmg(), player.getShieldRng(), player.getDodge()));
     // implement city fuctions here, talk to people, weapon shop, practice arena, instructions
+    // battle, practice arena, weapon shop
+    // talk to locals, instructions
+    function talkToLocals()
+    {
+      
+    }
+    function goToWeaponShop()
+    {
+      
+    }
+    function battle()
+    {
+      
+    }
+    function practiceArena()
+    {
+      
+    }
+    function viewInstructions()
+    {
+      
+    }
   }
 }
