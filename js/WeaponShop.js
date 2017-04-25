f67e468c13406a37d7d7a441f1360b74fbc4919b
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2017-04-24 22:59:01 -0700

    returns stats, wpn costs

--- js/WeaponShop.js
+++ js/WeaponShop.js
@@ -1,9 +1,5 @@
 class WeaponShop
 {
-  constructor(player)
-  {
-    this.player = player;
-  }
   buy()
   {
     
@@ -12,13 +8,15 @@ class WeaponShop
   {
     
   }
-  stats(weapon)
+  stats(player, weapon)
   {
-    var lightWpnRangeTimesPlayerLevel = (this.player.getLevel()*2);
-    var mediumWpnRangeTimesPlayerLevel = (this.player.getLevel()*3);
-    var heavyWpnRangeTimesPlayerLevel = (this.player.getLevel()*4);
-    var shieldRangeTimesPlayerLevel = (this.player.getLevel()*5);
-    var magicDmgRangeTimesPlayerLevel = (this.player.getLevel()*10);
+    console.log(player);
+    console.log("player level: "+player.getLevel());
+    var lightWpnRangeTimesPlayerLevel = (player.getLevel()*2);
+    var mediumWpnRangeTimesPlayerLevel = (player.getLevel()*3);
+    var heavyWpnRangeTimesPlayerLevel = (player.getLevel()*4);
+    var shieldRangeTimesPlayerLevel = (player.getLevel()*5);
+    var magicDmgRangeTimesPlayerLevel = (player.getLevel()*10);
     switch (weapon)
     {
       // light
@@ -85,12 +83,98 @@ class WeaponShop
         return (25 + shieldRangeTimesPlayerLevel);
       case "Mystical Shield":
         return (31 + shieldRangeTimesPlayerLevel);
+      // magic
       case "Ice Blast":
         return (10 + magicDmgRangeTimesPlayerLevel);
       case "Fire Blast":
         return (25 + magicDmgRangeTimesPlayerLevel);
       case "Lightning Blast":
         return (40 + magicDmgRangeTimesPlayerLevel);
+    }
+  }
+  price(weapon)
+  {
+    // return gold amount to buy weapon
+    switch (weapon)
+    {
+      // light
+      case "Blow Darts":
+        return (0);
+      case "Throwing Knives":
+        return (10);
+      case "Throwing Axe":
+        return (20);
+      case "Crossbow":
+        return (30);
+      case "Javelin":
+        return (40);
+      case "Recurve Bow":
+        return (50);
+      case "Long Bow":
+        return (60);
+      case "Mystical Bow":
+        return (70);
+      // medium weapons
+      case "Chain Whip":
+        return (0);
+      case "Ring Sword":
+        return (30);
+      case "Staff":
+        return (50);
+      case "Cestus":
+        return (70);
+      case "Spiked Chain":
+        return (90);
+      case "Meteor Hammer":
+        return (110);
+      case "Mace":
+        return (130);
+      case "Flail":
+        return (150);
+      // heavy weapons
+      case "Shortsword":
+        return (0);
+      case "War Hammer":
+        return (70);
+      case "Spear":
+        return (100);
+      case "Trident":
+        return (130);
+      case "Scythe":
+        return (160);
+      case "Battle Axe":
+        return (190);
+      case "Broadsword":
+        return (220);
+      case "Mystical Sword":
+        return (250);
+      // shields
+      case "Wood Shield":
+        return (0);
+      case "Iron Shield":
+        return (30);
+      case "Steel Shield":
+        return (60);
+      case "Silver Shield":
+        return (90);
+      case "Gold Shield":
+        return (120);
+      case "Mystical Shield":
+        return (150);
+      // power ups
+      case "Ice ":
+        return (100);
+      case "Fire ":
+        return (300);
+      case "Lightning ":
+        return (500);
+      // magic
+      case "Ice Blast":
+        return (200);
+      case "Fire Blast":
+        return (400);
+      case "Lightning Blast":
+        return (600);
     }
   }
 }
